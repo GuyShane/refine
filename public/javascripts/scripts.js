4c2307aec649a7c3dcab571f3c19c6195248a07f
Shane <shanebrass1618@gmail.com>
2016-05-29 23:04:04 -0600

    Consolidated javascripts. Started adding contact form validation

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,98 @@
+$(document).ready(()=>{
+
+    //Footer align
+    (function(){
+	align_footer();
+	
+	$(window).resize(function(){
+	    align_footer();
+	});
+
+	function align_footer(){
+	    const pad=parseInt($('.footer').css('height'))+100;
+	    $('.content').css('padding-bottom',pad);
+	}	
+    })();
+
+    //Gallery
+    (function(){
+	let idx=0;
+	const imgs=[];
+
+	$('.thumb').each(function(){
+	    let s=$(this).attr('src');
+	    s=s.split('/');
+	    imgs.push('images/gallery/pictures/'+s[s.length-1].replace('_thumb',''));
+	});
+	
+	$('.thumb').on('click',function(){
+	    let s=$(this).attr('src');
+	    idx=$('.thumb').index($(this));
+	    load_imgs(idx);
+	    s=s.split('/');
+	    s='images/gallery/pictures/'+s[s.length-1].replace('_thumb','');
+	    $('#modal-image').attr('src',s);
+	});
+
+	$(document).on('keyup',function(event){
+	    if (!$('#modal-image').is(':hidden')){
+		if (event.which==37){
+		    prev_img();
+		}
+		else if(event.which==39){
+		    next_img();
+		}
+	    }
+	});
+
+	$('#next-img').on('click',function(){
+	    next_img(idx);
+	});
+
+	$('#prev-img').on('click',function(){
+	    prev_img();
+	});
+	
+	function next_img(){
+	    idx=(idx+1)%imgs.length;
+	    change_img(idx);
+	}
+
+	function prev_img(){
+	    idx=(idx-1)%imgs.length;
+	    if (idx<0){idx=imgs.length-1;}
+	    change_img(idx);
+	}
+
+	function change_img(i){
+	    $('#modal-image').fadeTo(160,0.4,function(){
+		$('#modal-image').attr('src',imgs[i]);
+		$('#modal-image').fadeTo(160,1);
+	    });
+	    load_imgs(i);
+	}
+
+	function load_imgs(i){
+	    i_p=norm_idx(i-1);
+	    i_n=norm_idx(i+1);
+	    $('#load-prev').attr('src',imgs[i_p]);
+	    $('#load-next').attr('src',imgs[i_n]);
+	}
+
+	function norm_idx(i){
+	    if (i<0){return imgs.length-1;}
+	    return i%imgs.length;
+	}
+    })();
+
+    //Contact
+    (function(){
+	$('#contact-form').on('submit',function(){
+	    if (!captcha){return false;}
+	});
+    })();
+});
+let captcha=false;
+function captcha_submit(){
+    captcha=true;
+}
